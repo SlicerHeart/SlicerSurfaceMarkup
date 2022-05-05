3c1d068c87286d54bc2b2a2e871af4878010edba
Csaba Pinter <pinter.csaba@gmail.com>
2022-05-05 11:35:40 +0100

    STYLE: Remove ported Python code from NURBS source algorithm
    After having the NURBS source established and then improved, the commented out Python code for each ported function is not necessary anymore. Removing it will improve readability of the code and it will be easier to manage due to the fewer lines in the file.

--- GridSurfaceMarkups/VTKWidgets/vtkNURBSSurfaceSource.cxx
+++ GridSurfaceMarkups/VTKWidgets/vtkNURBSSurfaceSource.cxx
@@ -115,43 +115,6 @@ int vtkNURBSSurfaceSource::RequestData(
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::ComputeNurbsPolyData(vtkPoints* inputPoints, vtkPolyData* outputPolyData) // (points, size_u, size_v, degree_u, degree_v, **kwargs)
 {
-  // # Keyword arguments
-  // use_centripetal = kwargs.get('centripetal', False)
-  //
-  // # Get uk and vl
-  // uk, vl = compute_params_surface(points, size_u, size_v, use_centripetal)
-  //
-  // # Compute knot vectors
-  // kv_u = compute_knot_vector(degree_u, size_u, uk)
-  // kv_v = compute_knot_vector(degree_v, size_v, vl)
-  //
-  // # Do global interpolation on the u-direction
-  // ctrlpts_r = []
-  // for v in range(size_v):
-  //     pts = [points[v + (size_v * u)] for u in range(size_u)] #CP_Note: Get vth column of points
-  //     matrix_a = _build_coeff_matrix(degree_u, kv_u, uk, pts)
-  //     ctrlpts_r += linalg.lu_solve(matrix_a, pts)
-  //
-  // # Do global interpolation on the v-direction
-  // ctrlpts = []
-  // for u in range(size_u):
-  //     pts = [ctrlpts_r[u + (size_u * v)] for v in range(size_v)]
-  //     matrix_a = _build_coeff_matrix(degree_v, kv_v, vl, pts)
-  // #CP_Note: linalg.lu_solve
-  //     ctrlpts += linalg.lu_solve(matrix_a, pts)
-  //
-  // # Generate B-spline surface
-  // surf = BSpline.Surface()
-  // surf.degree_u = degree_u
-  // surf.degree_v = degree_v
-  // surf.ctrlpts_size_u = size_u
-  // surf.ctrlpts_size_v = size_v
-  // surf.ctrlpts = ctrlpts
-  // surf.knotvector_u = kv_u
-  // surf.knotvector_v = kv_v
-  //
-  // return surf
-
   if (outputPolyData == nullptr)
   {
     return;
@@ -255,51 +218,6 @@ void vtkNURBSSurfaceSource::ComputeNurbsPolyData(vtkPoints* inputPoints, vtkPoly
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::EvaluateSurface(vtkDoubleArray* uKnots, vtkDoubleArray* vKnots, vtkPoints* controlPoints, vtkPoints* outEvalPoints)
 {
-  // :param datadict: data dictionary containing the necessary variables
-  // :type datadict: dict
-  // :return: evaluated points
-  // :rtype: list
-  // """
-  // # Geometry data from datadict
-  // sample_size = datadict['sample_size'] #CP_Note: Inverse of delta
-  // degree = datadict['degree']
-  // knotvector = datadict['knotvector']
-  // ctrlpts = datadict['control_points']
-  // size = datadict['size'] #CP_Note: InputResolution
-  // dimension = datadict['dimension'] + 1 if datadict['rational'] else datadict['dimension'] #CP_Note: 3
-  // pdimension = datadict['pdimension'] #CP_Note: 2
-  // precision = datadict['precision']
-  //
-  // # Keyword arguments
-  // start = kwargs.get('start', [0.0 for _ in range(pdimension)])
-  // stop = kwargs.get('stop', [1.0 for _ in range(pdimension)])
-  //
-  // # Algorithm A3.5
-  // spans = [[] for _ in range(pdimension)]
-  // basis = [[] for _ in range(pdimension)]
-  // for idx in range(pdimension):
-  // #CP_Note: linalg.linspace, helpers.find_spans, helpers.basis_functions
-  //     knots = linalg.linspace(start[idx], stop[idx], sample_size[idx], decimals=precision)
-  //     spans[idx] = helpers.find_spans(degree[idx], knotvector[idx], size[idx], knots, self._span_func)
-  //     basis[idx] = helpers.basis_functions(degree[idx], knotvector[idx], spans[idx], knots)
-  //
-  // eval_points = []
-  // for i in range(len(spans[0])):
-  //     idx_u = spans[0][i] - degree[0]
-  //     for j in range(len(spans[1])):
-  //         idx_v = spans[1][j] - degree[1]
-  //         spt = [0.0 for _ in range(dimension)]
-  //         for k in range(0, degree[0] + 1):
-  //             temp = [0.0 for _ in range(dimension)]
-  //             for l in range(0, degree[1] + 1):
-  //                 temp[:] = [tmp + (basis[1][j][l] * cp) for tmp, cp in
-  //                            zip(temp, ctrlpts[idx_v + l + (size[1] * (idx_u + k))])]
-  //             spt[:] = [pt + (basis[0][i][k] * tmp) for pt, tmp in zip(spt, temp)]
-  //
-  //         eval_points.append(spt)
-  //
-  // return eval_points
-
   if (this->Delta < 0.001)
   {
     vtkErrorMacro("EvaluateSurface: Delta value too small");
@@ -453,50 +371,6 @@ void vtkNURBSSurfaceSource::TriangulateSurface(vtkPolyData* outputPolyData)
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::ComputeParamsSurface(vtkPoints* inputPoints, vtkDoubleArray* ukParams, vtkDoubleArray* vlParams)
 {
-  // :param points: data points
-  // :type points: list, tuple
-  // :param size_u: number of points on the u-direction
-  // :type size_u: int
-  // :param size_v: number of points on the v-direction
-  // :type size_v: int
-  // :param centripetal: activates centripetal parametrization method
-  // :type centripetal: bool
-  // :return: :math:`\\overline{u}_{k}` and :math:`\\overline{u}_{l}` parameter arrays as a tuple
-  // :rtype: tuple
-  // """
-  // # Compute uk
-  // uk = [0.0 for _ in range(size_u)]
-  //
-  // # Compute for each curve on the v-direction
-  // uk_temp = []
-  // for v in range(size_v):
-  // #CP_Note: Get v'th column
-  //     pts_u = [points[v + (size_v * u)] for u in range(size_u)]
-  // #CP_Note: Append parameter list to one long list
-  //     uk_temp += compute_params_curve(pts_u, centripetal)
-  //
-  // # Do averaging on the u-direction
-  // for u in range(size_u):
-  //     knots_v = [uk_temp[u + (size_u * v)] for v in range(size_v)]
-  //     uk[u] = sum(knots_v) / size_v
-  //
-  // # Compute vl
-  // vl = [0.0 for _ in range(size_v)]
-  //
-  // # Compute for each curve on the u-direction
-  // vl_temp = []
-  // #CP_Note: Get u'th row
-  // for u in range(size_u):
-  //     pts_v = [points[v + (size_v * u)] for v in range(size_v)]
-  //     vl_temp += compute_params_curve(pts_v, centripetal)
-  //
-  // # Do averaging on the v-direction
-  // for v in range(size_v):
-  //     knots_u = [vl_temp[v + (size_v * u)] for u in range(size_u)]
-  //     vl[v] = sum(knots_u) / size_u
-  //
-  // return uk, vl
-
   // Account for points included for wrapping around
   int interpolatingOverlap[2] = {0};
   this->GetInterpolatingOverlap(interpolatingOverlap);
@@ -562,37 +436,6 @@ void vtkNURBSSurfaceSource::ComputeParamsSurface(vtkPoints* inputPoints, vtkDoub
 void vtkNURBSSurfaceSource::ComputeParamsCurve(
   vtkPoints* inputPoints, vtkIdList* pointIndexList, vtkDoubleArray* outParametersArray)
 {
-  // :param points: data points
-  // :type points: list, tuple
-  // :param centripetal: activates centripetal parametrization method
-  // :type centripetal: bool
-  // :return: parameter array, :math:`\\overline{u}_{k}`
-  // :rtype: list
-  // """
-  // if not isinstance(points, (list, tuple)):
-  //     raise TypeError("Data points must be a list or a tuple")
-  //
-  // # Length of the points array
-  // num_points = len(points)
-  //
-  // # Calculate chord lengths
-  // cds = [0.0 for _ in range(num_points + 1)]
-  // cds[-1] = 1.0
-  // for i in range(1, num_points):
-  // #CP_Note: linalg.point_distance
-  //     distance = linalg.point_distance(points[i], points[i - 1])
-  //     cds[i] = math.sqrt(distance) if centripetal else distance
-  //
-  // # Find the total chord length
-  // d = sum(cds[1:-1])
-  //
-  // # Divide individual chord lengths by the total chord length
-  // uk = [0.0 for _ in range(num_points)]
-  // for i in range(num_points):
-  //     uk[i] = sum(cds[0:i + 1]) / d
-  //
-  // return uk
-
   if (!pointIndexList || !outParametersArray)
   {
     vtkErrorMacro("ComputeParamsCurve: Invalid arguments");
@@ -634,28 +477,6 @@ void vtkNURBSSurfaceSource::ComputeParamsCurve(
 void vtkNURBSSurfaceSource::ComputeKnotVector(
   int degree, int numOfPoints, vtkDoubleArray* inParams, vtkDoubleArray* outKnotVector)
 {
-  // :param degree: degree
-  // :type degree: int
-  // :param num_points: number of data points
-  // :type num_points: int
-  // :param params: list of parameters, :math:`\\overline{u}_{k}`
-  // :type params: list, tuple
-  // :return: knot vector
-  // :rtype: list
-  // """
-  // # Start knot vector
-  // kv = [0.0 for _ in range(degree + 1)]
-  //
-  // # Use averaging method (Eqn 9.8) to compute internal knots in the knot vector
-  // for i in range(num_points - degree - 1):
-  //     temp_kv = (1.0 / degree) * sum([params[j] for j in range(i + 1, i + degree + 1)])
-  //     kv.append(temp_kv)
-  //
-  // # End knot vector
-  // kv += [1.0 for _ in range(degree + 1)]
-  //
-  // return kv
-
   if (degree == 0 || numOfPoints == 0)
   {
     vtkErrorMacro("ComputeKnotVector: Input values should be different than zero");
@@ -703,31 +524,6 @@ void vtkNURBSSurfaceSource::ComputeKnotVector(
 void vtkNURBSSurfaceSource::BuildCoeffMatrix(
   int degree, vtkDoubleArray* knotVector, vtkDoubleArray* params, vtkPoints* points, double** outCoeffMatrix)
 {
-  // :param degree: degree
-  // :type degree: int
-  // :param knotvector: knot vector
-  // :type knotvector: list, tuple
-  // :param params: list of parameters
-  // :type params: list, tuple
-  // :param points: data points
-  // :type points: list, tuple
-  // :return: coefficient matrix
-  // :rtype: list
-  // """
-  // # Number of data points
-  // num_points = len(points)
-  //
-  // # Set up coefficient matrix
-  // matrix_a = [[0.0 for _ in range(num_points)] for _ in range(num_points)]
-  // for i in range(num_points):
-  // #CP_Note: helpers.find_span_linear
-  //     span = helpers.find_span_linear(degree, knotvector, num_points, params[i])
-  // #CP_Note: helpers.basis_function
-  //     matrix_a[i][span-degree:span+1] = helpers.basis_function(degree, knotvector, span, params[i])
-  //
-  // # Return coefficient matrix
-  // return matrix_a
-
   if (degree == 0)
   {
     vtkErrorMacro("BuildCoeffMatrix: Invalid input degree");
@@ -775,33 +571,6 @@ void vtkNURBSSurfaceSource::BuildCoeffMatrix(
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::BasisFunction(int degree, vtkDoubleArray* knotVector, int span, double knot, vtkDoubleArray* outBasisFunctions)
 {
-  // :param degree: degree, :math:`p`
-  // :type degree: int
-  // :param knot_vector: knot vector, :math:`U`
-  // :type knot_vector: list, tuple
-  // :param span: knot span, :math:`i`
-  // :type span: int
-  // :param knot: knot or parameter, :math:`u`
-  // :type knot: float
-  // :return: basis functions
-  // :rtype: list
-  // """
-  // left = [0.0 for _ in range(degree + 1)]
-  // right = [0.0 for _ in range(degree + 1)]
-  // N = [1.0 for _ in range(degree + 1)]  # N[0] = 1.0 by definition
-  //
-  // for j in range(1, degree + 1):
-  //     left[j] = knot - knot_vector[span + 1 - j]
-  //     right[j] = knot_vector[span + j] - knot
-  //     saved = 0.0
-  //     for r in range(0, j):
-  //         temp = N[r] / (right[r + 1] + left[j - r])
-  //         N[r] = saved + right[r + 1] * temp
-  //         saved = left[j - r] * temp
-  //     N[j] = saved
-  //
-  // return N
-
   if (!outBasisFunctions)
   {
     vtkErrorMacro("BasisFunction: Invalid output array");
@@ -843,22 +612,6 @@ void vtkNURBSSurfaceSource::BasisFunction(int degree, vtkDoubleArray* knotVector
 void vtkNURBSSurfaceSource::BasisFunctions(
   int degree, vtkDoubleArray* knotVector, vtkIntArray* spans, vtkDoubleArray* knots, vtkDoubleArray* outBasisFunctions)
 {
-  // :param degree: degree, :math:`p`
-  // :type degree: int
-  // :param knot_vector: knot vector, :math:`U`
-  // :type knot_vector: list, tuple
-  // :param spans: list of knot spans
-  // :type spans:  list, tuple
-  // :param knots: list of knots or parameters
-  // :type knots: list, tuple
-  // :return: basis functions
-  // :rtype: list
-  // """
-  // basis = []
-  // for span, knot in zip(spans, knots):
-  //     basis.append(basis_function(degree, knot_vector, span, knot))
-  // return basis
-
   if (!knotVector || !spans || !knots)
   {
     vtkErrorMacro("BasisFunctions: Invalid input arrays");
@@ -890,23 +643,6 @@ void vtkNURBSSurfaceSource::BasisFunctions(
 //---------------------------------------------------------------------------
 int vtkNURBSSurfaceSource::FindSpanLinear(int degree, vtkDoubleArray* knotVector, int numControlPoints, double knot)
 {
-  // :param degree: degree, :math:`p`
-  // :type degree: int
-  // :param knot_vector: knot vector, :math:`U`
-  // :type knot_vector: list, tuple
-  // :param num_ctrlpts: number of control points, :math:`n + 1`
-  // :type num_ctrlpts: int
-  // :param knot: knot or parameter, :math:`u`
-  // :type knot: float
-  // :return: knot span
-  // :rtype: int
-  // """
-  // span = degree + 1  # Knot span index starts from zero
-  // while span < num_ctrlpts and knot_vector[span] <= knot:
-  //     span += 1
-  //
-  // return span - 1
-
   // Knot span index starts from zero
   int span = degree + 1;
 
@@ -921,23 +657,6 @@ int vtkNURBSSurfaceSource::FindSpanLinear(int degree, vtkDoubleArray* knotVector
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::FindSpans(int degree, vtkDoubleArray* knotVector, int numControlPoints, vtkDoubleArray* knots, vtkIntArray* outSpans)
 {
-  // :param degree: degree, :math:`p`
-  // :type degree: int
-  // :param knot_vector: knot vector, :math:`U`
-  // :type knot_vector: list, tuple
-  // :param num_ctrlpts: number of control points, :math:`n + 1`
-  // :type num_ctrlpts: int
-  // :param knots: list of knots or parameters
-  // :type knots: list, tuple
-  // :param func: function for span finding, e.g. linear or binary search
-  // :return: list of spans
-  // :rtype: list
-  // """
-  // spans = []
-  // for knot in knots:
-  //     spans.append(func(degree, knot_vector, num_ctrlpts, knot))
-  // return spans
-
   if (!knotVector || !knots)
   {
     vtkErrorMacro("FindSpans: Invalid input arrays");
@@ -963,32 +682,6 @@ void vtkNURBSSurfaceSource::FindSpans(int degree, vtkDoubleArray* knotVector, in
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::LuSolve(double** coeffMatrix, int matrixSize, vtkPoints* points, vtkPoints* outControlPointsR)
 {
-  // :param matrix_a: matrix A
-  // :type m_l: list
-  // :param b: matrix of M column vectors
-  // :type b: list
-  // :return: x, the solution matrix
-  // :rtype: list
-  // """
-  // # Variable initialization
-  // dim = len(b[0])
-  // num_x = len(b)
-  // x = [[0.0 for _ in range(dim)] for _ in range(num_x)]
-  //
-  // # LU decomposition
-  // m_l, m_u = lu_decomposition(matrix_a)
-  //
-  // # Solve the system of linear equations
-  // for i in range(dim):
-  //     bt = [b1[i] for b1 in b]
-  //     y = forward_substitution(m_l, bt)
-  //     xt = backward_substitution(m_u, y)
-  //     for j in range(num_x):
-  //         x[j][i] = xt[j]
-  //
-  // # Return the solution
-  // return x
-
   if (!coeffMatrix || !points)
   {
     vtkErrorMacro("LuSolve: Invalid input coefficients matrix or points");
@@ -1048,23 +741,6 @@ void vtkNURBSSurfaceSource::LuSolve(double** coeffMatrix, int matrixSize, vtkPoi
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::LuDecomposition(double** matrixA, double** matrixL, double** matrixU, int size)
 {
-  // :param matrix_a: Input matrix (must be a square matrix)
-  // :type matrix_a: list, tuple
-  // :return: a tuple containing matrices L and U
-  // :rtype: tuple
-  // """
-  // # Check if the 2-dimensional input matrix is a square matrix
-  // q = len(matrix_a)
-  // for idx, m_a in enumerate(matrix_a):
-  //     if len(m_a) != q:
-  //         raise ValueError("The input must be a square matrix. " +
-  //                          "Row " + str(idx + 1) + " has a size of " + str(len(m_a)) + ".")
-  //
-  // # Return L and U matrices
-  // return _linalg.doolittle(matrix_a)
-
-  // Porting note: L and U matrices differ from the Python implementation but their product is exactly matrix A
-
   if (!matrixA || !matrixL || !matrixU)
   {
     vtkErrorMacro("LuDecomposition: Invalid input matrices");
@@ -1113,21 +789,6 @@ void vtkNURBSSurfaceSource::LuDecomposition(double** matrixA, double** matrixL, 
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::ForwardSubstitution(double** matrixL, double* b, int size, double* outY)
 {
-  // :param matrix_l: L, lower triangular matrix
-  // :type matrix_l: list, tuple
-  // :param matrix_b: b, column matrix
-  // :type matrix_b: list, tuple
-  // :return: y, column matrix
-  // :rtype: list
-  // """
-  // q = len(matrix_b)
-  // matrix_y = [0.0 for _ in range(q)]
-  // matrix_y[0] = float(matrix_b[0]) / float(matrix_l[0][0])
-  // for i in range(1, q):
-  //     matrix_y[i] = float(matrix_b[i]) - sum([matrix_l[i][j] * matrix_y[j] for j in range(0, i)])
-  //     matrix_y[i] /= float(matrix_l[i][i])
-  // return matrix_y
-
   if (!matrixL || !b || !outY)
   {
     vtkErrorMacro("ForwardSubstitution: Invalid input/output arguments");
@@ -1155,21 +816,6 @@ void vtkNURBSSurfaceSource::ForwardSubstitution(double** matrixL, double* b, int
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::BackwardSubstitution(double** matrixU, double* y, int size, double* outX)
 {
-  // :param matrix_u: U, upper triangular matrix
-  // :type matrix_u: list, tuple
-  // :param matrix_y: y, column matrix
-  // :type matrix_y: list, tuple
-  // :return: x, column matrix
-  // :rtype: list
-  // """
-  // q = len(matrix_y)
-  // matrix_x = [0.0 for _ in range(q)]
-  // matrix_x[q - 1] = float(matrix_y[q - 1]) / float(matrix_u[q - 1][q - 1])
-  // for i in range(q - 2, -1, -1):
-  //     matrix_x[i] = float(matrix_y[i]) - sum([matrix_u[i][j] * matrix_x[j] for j in range(i, q)])
-  //     matrix_x[i] /= float(matrix_u[i][i])
-  // return matrix_x
-
   if (!matrixU || !y || !outX)
   {
     vtkErrorMacro("BackwardSubstitution: Invalid input/output arguments");
@@ -1197,29 +843,6 @@ void vtkNURBSSurfaceSource::BackwardSubstitution(double** matrixU, double* y, in
 //---------------------------------------------------------------------------
 void vtkNURBSSurfaceSource::LinSpace(double start, double stop, int numOfSamples, vtkDoubleArray* outSpace)
 {
-  // :param start: starting value
-  // :type start: float
-  // :param stop: end value
-  // :type stop: float
-  // :param num: number of samples to generate
-  // :type num: int
-  // :param decimals: number of significands
-  // :type decimals: int
-  // :return: a list of equally spaced numbers
-  // :rtype: list
-  // """
-  // start = float(start)
-  // stop = float(stop)
-  // if abs(start - stop) <= 10e-8:
-  //     return [start]
-  // num = int(num)
-  // if num > 1:
-  //     div = num - 1
-  //     delta = stop - start
-  //     return [float(("{:." + str(decimals) + "f}").format((start + (float(x) * float(delta) / float(div)))))
-  //             for x in range(num)]
-  // return [float(("{:." + str(decimals) + "f}").format(start))]
-
   if (!outSpace)
   {
     vtkErrorMacro("LinSpace: Invalid output array");
